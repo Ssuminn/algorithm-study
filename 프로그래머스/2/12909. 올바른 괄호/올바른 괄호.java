629620e09e58f7011942e8990ecdedbae8a72b71
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-03-26 21:49:58 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -9,16 +9,16 @@ class Solution {
         
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i)=='(') {
-                stack.push('(');
+                stack.push('('); // 스택에 '('을 넣어줌
             }
             else if(s.charAt(i)==')'){
-                if(stack.isEmpty()) {
+                if(stack.isEmpty()) { // ')'로 시작시 올바르지 않은 괄호
                     return false;
                 }
-                stack.pop();
+                stack.pop(); // 스택에 들어있는'('을 빼줌
             }
         }
 
-        return stack.isEmpty();
+        return stack.isEmpty(); 
     }
 }
