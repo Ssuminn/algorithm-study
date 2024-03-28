d2cd24004f6ab132759eb83b08cd20c65cf80421
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-03-29 00:56:24 +0900

    Update 2016년.java

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int a, int b) {
         String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"}; // 1월 1일은 금요일
-        int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] months = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 1~12월 의 일수
         int date = 0; // 총 일수
         
-        for(int i = 0; i < a - 1; i++) { // a월전까지의 총일수 구하기
-            date += months[i];
+        for(int i = 0; i < a - 1; i++) { 
+            date += months[i]; // a월전까지의 총일수 구하기
         }
         date += b - 1; // b의 일수 만큼 더 더해주기
 
