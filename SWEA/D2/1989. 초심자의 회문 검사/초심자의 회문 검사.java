205d3362f46f91fc865291f16a0fe767fc409d57
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-04-17 11:37:29 +0900

    [D2] Title: 초심자의 회문 검사, Time: 130 ms, Memory: 27,120 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+        
+        for(int test_case = 1; test_case <= T; test_case++) {
+            String word = sc.next();
+            int answer = 1; // 기본적으로 회문이라고 가정
+            
+            for(int i = 0; i < word.length() / 2; i++) {
+                if(word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                    answer = 0; // 회문이 아닌 경우
+                    break; // 더 이상 비교할 필요 없음
+                }
+            }
+            System.out.println("#" + test_case + " " + answer);
+        }
+    }
+}
