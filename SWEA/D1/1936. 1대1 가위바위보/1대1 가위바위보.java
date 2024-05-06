5bdb4647c3b46c1d47f287daed494da0db5d3f65
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-05-06 16:30:23 +0900

    Update 1대1 가위바위보.java

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -22,3 +22,37 @@ class Solution {
         System.out.println(result);
     }
 }
+
+// // A가 가위(1)를 냈을 때
+//         if (A == 1) {
+//             // B가 바위(2)를 냈을 때 B가 이김
+//             if (B == 2) {
+//                 System.out.println("B");
+//             }
+//             // B가 보(3)를 냈을 때 A가 이김
+//             else {
+//                 System.out.println("A");
+//             }
+//         }
+//         // A가 바위(2)를 냈을 때
+//         else if (A == 2) {
+//             // B가 가위(1)를 냈을 때 A가 이김
+//             if (B == 1) {
+//                 System.out.println("A");
+//             }
+//             // B가 보(3)를 냈을 때 B가 이김
+//             else {
+//                 System.out.println("B");
+//             }
+//         }
+//         // A가 보(3)를 냈을 때
+//         else {
+//             // B가 가위(1)를 냈을 때 B가 이김
+//             if (B == 1) {
+//                 System.out.println("B");
+//             }
+//             // B가 바위(2)를 냈을 때 A가 이김
+//             else {
+//                 System.out.println("A");
+//             }
+//         }
