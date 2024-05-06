0163a184438493a52a57cef971c64b76584d9a4b
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-05-06 16:29:10 +0900

    [D1] Title: 1대1 가위바위보, Time: 130 ms, Memory: 26,216 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -1,47 +1,24 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-	
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
         int B = sc.nextInt();
         
-        // A가 가위(1)를 냈을 때
+        String result = "";
         if (A == 1) {
-            // B가 바위(2)를 냈을 때 B가 이김
-            if (B == 2) {
-                System.out.println("B");
-            }
-            // B가 보(3)를 냈을 때 A가 이김
-            else {
-                System.out.println("A");
-            }
-        }
-        // A가 바위(2)를 냈을 때
+            if (B % 2 == 0) result = "B";
+            else if (B % 2 == 1) result = "A";
+        } 
         else if (A == 2) {
-            // B가 가위(1)를 냈을 때 A가 이김
-            if (B == 1) {
-                System.out.println("A");
-            }
-            // B가 보(3)를 냈을 때 B가 이김
-            else {
-                System.out.println("B");
-            }
+            if (B % 3 == 1) result = "A";
+            else if (B % 3 == 0) result = "B";
+        } 
+        else if (A == 3) {
+            if (B % 2 == 1) result = "B";
+            else if (B % 2 == 0) result = "A";
         }
-        // A가 보(3)를 냈을 때
-        else {
-            // B가 가위(1)를 냈을 때 B가 이김
-            if (B == 1) {
-                System.out.println("B");
-            }
-            // B가 바위(2)를 냈을 때 A가 이김
-            else {
-                System.out.println("A");
-            }
-        }
-	}
+        System.out.println(result);
+    }
 }
