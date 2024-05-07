c509dfbc2426c2a090946f75ca516b02ad4cf8fb
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-05-07 22:56:32 +0900

    [D3] Title: 구구단2, Time: 148 ms, Memory: 26,620 KB -BaekjoonHub

---
+++ SWEA/D3/12221. 구구단2/구구단2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+	
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = 0;
+            
+            if(a < 10 && b < 10){
+            	sum = a*b;
+            }
+            else{
+            	sum = -1;
+            }
+            System.out.println("#" + test_case + " " + sum);
+		}
+	}
+}
