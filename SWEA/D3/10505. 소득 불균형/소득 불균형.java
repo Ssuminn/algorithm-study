5e354b434bfa04cd80060f93cd4c358079c61805
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-05-09 23:23:09 +0900

    Update 소득 불균형.java

--- SWEA/D3/10505. 소득 불균형/소득 불균형.java
+++ SWEA/D3/10505. 소득 불균형/소득 불균형.java
@@ -13,22 +13,24 @@ class Solution
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			int N = sc.nextInt();
-            int[] n = new int [N];
-            int sum = 0;
-            int avg = 0;
-            int count = 0;
-            for(int i = 0; i < N; i++) {
-                n[i] = sc.nextInt();
+            		int[] n = new int [N];
+            		int sum = 0;
+           		int avg = 0;
+           		int count = 0;
+			
+            		for(int i = 0; i < N; i++) {
+                		n[i] = sc.nextInt();
                 
-                sum += n[i];
-            }
-            avg = sum/N;
-            for(int i = 0; i < N; i++) {
-            	if(n[i]<=avg){
-                	count++;
-                }
-            }
-            System.out.println("#" + test_case + " " + count);
+                		sum += n[i];
+            		}
+            		avg = sum/N;
+			
+            		for(int i = 0; i < N; i++) {
+            			if(n[i]<=avg){
+                			count++;
+                		}
+            		}
+            	System.out.println("#" + test_case + " " + count);
 		}
 	}
 }
