7f0c07d75f42afc687e9010a7bee65d826a394d9
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-07-19 20:17:25 +0900

    [level 0] Title: 문자열로 변환, Time: 0.04 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        answer = Integer.toString(n);
+        // 문자열 + 정수 = 문자열
+        // answer = n + ""
+        
+        return answer;
+    }
+}
