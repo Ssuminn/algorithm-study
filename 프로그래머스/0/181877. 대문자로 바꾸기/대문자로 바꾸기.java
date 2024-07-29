50e710a8d21d0cf192734a38a51b0c3518e72ba5
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-07-29 23:17:07 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        // 인덱스의 문자열을 대문자로 변환
+        answer = myString.toUpperCase();
+        
+        return answer;
+    }
+}
