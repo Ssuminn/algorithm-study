8e4bbed0297c55d1a9af262854992b43c44c4ed7
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-07-29 23:14:02 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.04 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        // 인덱스의 문자열을 소문자로 변환
+        answer = myString.toLowerCase();
+        
+        return answer;
+    }
+}
