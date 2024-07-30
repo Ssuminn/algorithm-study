585e36b124ebbf9497c108d04f8cb2bfffae719f
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-07-30 18:08:53 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        // 배열의 크기 계산
+        int size = (num_list.length + n - 1) / n;
+        int[] answer = new int[size];
+        
+        for(int i = 0; i < size; i++) {
+            answer[i] = num_list[i * n];
+        }
+        
+        return answer;
+    }
+}
