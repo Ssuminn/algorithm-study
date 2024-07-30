4292f7bdaddc08bc6ee6bd25666e39293debf456
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-07-30 18:54:51 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.14 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] solution(int n, int k) {
-        int[] answer = new int[n/k];
+        int size = n/k;
+        int[] answer = new int[size];
         
         for(int i = 0; i < answer.length; i++) {
             answer[i] = k * (i + 1);
