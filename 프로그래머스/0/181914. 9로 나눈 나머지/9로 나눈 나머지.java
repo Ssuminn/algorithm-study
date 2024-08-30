bd2cff56942929b53b55ffddafe74313422d1385
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-08-30 15:21:35 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.66 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -4,6 +4,7 @@ class Solution {
         
         // 문자열의 각 자릿수의 합
         for (int i = 0; i < number.length(); i++) {
+            // - '0'을 통해 문자를 숫자로 변환
             answer += number.charAt(i) - '0';
         }
         
