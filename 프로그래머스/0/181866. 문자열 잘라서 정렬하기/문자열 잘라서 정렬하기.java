cbc846b4d2e3744b3af4ced4ac9db184be915176
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-09-05 17:51:26 +0900

    문자열 잘라서 정렬하기.java

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -8,17 +8,15 @@ class Solution {
         ArrayList<String> list = new ArrayList<>();
         
         // strArr의 각 문자열을 확인하면서, 비어있지 않은 문자열이라면 list에 추가
-        for(String str : strArr){ // 2.
-            if(!str.isEmpty()){
+        for(String str : strArr) { 
+            if(!str.isEmpty()) {
                 list.add(str);
             }
         }
-
-        String[] answer =  list.toArray(new String[list.size()]);
-        // list.toArray(answer);
         
         // String[] answer = list.toArray(new String[0]);
-        
+        // 리스트를 배열로 변환
+        String[] answer =  list.toArray(new String[list.size()]);
         
         // 사전순 정렬
         Arrays.sort(answer);
