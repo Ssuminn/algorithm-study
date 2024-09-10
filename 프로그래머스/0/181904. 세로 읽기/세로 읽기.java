f816d9a130290794110142416a0546943dab5882
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-09-10 23:41:48 +0900

    세로 읽기.java

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -2,8 +2,10 @@ class Solution {
     public String solution(String my_string, int m, int c) {
         String answer = "";
         
-        for(int i = c-1; i < my_string.length(); i+=m) {
-                answer += my_string.charAt(i);
+        // i를 c-1로 시작하고, i가 m만큼씩 증가하면서 my_string의 범위 안에서 반복
+        for(int i = c - 1; i < my_string.length(); i += m) {
+            // 각 구간에서 c번째 문자 추출하여 answer에 추가
+            answer += my_string.charAt(i);
         }
         
         return answer;
