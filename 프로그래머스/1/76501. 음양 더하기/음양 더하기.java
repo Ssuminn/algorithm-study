04a21ddb5d375165d5ffceae9eb24dbb8cb5e7bf
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-09-11 21:54:42 +0900

    음양 더하기.java

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,13 +1,14 @@
-
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for (int i = 0; i < absolutes.length; i++) {
-            if(signs[i]){
-                answer +=absolutes[i];
+        for(int i = 0; i < absolutes.length; i++) {
+            // true
+            if(signs[i]) {
+                answer += absolutes[i];
             }
-            else{
+            // false
+            else {
                 answer -= absolutes[i];
             }
         }
