51b549017438f6bd9475d2099975375d843fe9d2
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-09-11 21:33:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,21 +1,25 @@
 class Solution {
-    public int solution(long num) { //형변환 필요 int =>long
+    public int solution(long num) {
         int answer = 0;
-            while(num!=1){
-                if(num%2==0){
-                    num/=2;
-                    answer++;
-                }
-                else{
-                    num=num*3+1;
-                    answer++;
-                }
-                if(answer>=500){
-                    answer= -1;
-                    break;
-                }
-                
+        
+        while(num != 1) {
+            // num이 짝수라면
+            if (num % 2 == 0) {
+                num /= 2;
             }
+            // num이 홀수라면
+            else {
+                num = num * 3 + 1;
+            }
+            answer++;  // 반복할 때마다 작업 횟수를 증가
+            
+            // 작업 반복 500회 이상인 경우
+            if(answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+        
         return answer;
     }
 }
