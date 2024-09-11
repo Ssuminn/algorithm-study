e875dc6af65915089a2d0e0cd85cca18a1a381d3
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-09-11 22:05:11 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,9 @@
 class Solution {
     public int solution(int[] numbers) {
+        // 0~9까지 전체 합
         int answer = 45;
         
+        // 전체에서 있는 숫자를 빼기 = 없는 숫자의 합
         for(int i = 0; i < numbers.length; i++) {
             answer -= numbers[i];
         }
