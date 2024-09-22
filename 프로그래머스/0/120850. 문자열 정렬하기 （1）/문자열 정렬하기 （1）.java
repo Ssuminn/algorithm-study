70b0d45beaa01ab81ec98f11e28daa7dd4a4ae18
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-09-22 23:55:12 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.80 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        // replaceAll([정규식],[바꿀문자])
+        my_string = my_string.replaceAll("[a-z]", "");
+        
+        String[] arr = my_string.split("");
+        int[] answer = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++) {
+            // 문자열 -> 숫자
+            answer[i] = Integer.parseInt(arr[i]);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
